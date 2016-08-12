9502bb4a6c2a446edf1f560852fed9b284ec6810
Florian Schliep <florian@appiculous.com>
2016-08-12 18:55:49 +0200

    Add Module Definition
    - Add umbrella header HeroicLabs.h
    - Change DEFINES_MODULE setting in Xcode project

---
+++ Classes/HeroicLabs.h
@@ -0,0 +1,47 @@
+/*
+ Copyright 2015-2016 Heroic Labs
+ 
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+ 
+ http://www.apache.org/licenses/LICENSE-2.0
+ 
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
+
+#ifndef HeroicLabs
+#define HeroicLabs
+
+#import <HeroicLabs/HLgzipRequestSerializer.h>
+#import <HeroicLabs/NSURLRequest+HLURLRequest.h>
+#import <HeroicLabs/HLHttpClient.h>
+#import <HeroicLabs/HLLeaderboard.h>
+#import <HeroicLabs/HLMatchChange.h>
+#import <HeroicLabs/HLRequestRetryHandlerProtocol.h>
+#import <HeroicLabs/HLGame.h>
+#import <HeroicLabs/HLSessionClient.h>
+#import <HeroicLabs/HLClient.h>
+#import <HeroicLabs/HLServer.h>
+#import <HeroicLabs/HLAchievement.h>
+#import <HeroicLabs/HLMatch.h>
+#import <HeroicLabs/HLPurchaseVerification.h>
+#import <HeroicLabs/NSData+GZIP.h>
+#import <HeroicLabs/HeroicLabs.h>
+#import <HeroicLabs/HLDefaultRequestRetryHandler.h>
+#import <HeroicLabs/HLMessage.h>
+#import <HeroicLabs/HLGameCenterCredentials.h>
+#import <HeroicLabs/HLNoRequestRetryHandler.h>
+#import <HeroicLabs/HLJSONSerialisableProtocol.h>
+#import <HeroicLabs/HLPing.h>
+#import <HeroicLabs/HLDatastoreObject.h>
+#import <HeroicLabs/HLSharedStorageSearchResults.h>
+#import <HeroicLabs/HLMatchTurn.h>
+#import <HeroicLabs/HLGamer.h>
+#import <HeroicLabs/HLLeaderboardRank.h>
+
+#endif
